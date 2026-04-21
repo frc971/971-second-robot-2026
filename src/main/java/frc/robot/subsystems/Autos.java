aeceb93beb4da760f6d5f3a3739b30f2e66412b6
AlexanderStrougo <118131066+AlexanderStrougo@users.noreply.github.com>
2026-04-20 17:06:55 -0700

    init test auto

--- src/main/java/frc/robot/subsystems/Autos.java
+++ src/main/java/frc/robot/subsystems/Autos.java
@@ -123,5 +123,8 @@ public class Autos {
           new AutoRoutine(
               "Niko No-depot", List.of("S_Niko", "H_Niko_Normal", "F_Niko2nd", "H_Niko_Normal")),
           new AutoRoutine("L_swipe", List.of("C_shoot", "L_swipe", "C_shoot", "L_swipe")),
-          new AutoRoutine("Kev", List.of("Kev")));
+          new AutoRoutine("Kev", List.of("Kev")),
+          new AutoRoutine(
+              "Test Continuity",
+              List.of("test_A", "test_B", "test_C", "test_D", "test_A", "test_B", "test_A")));
 }
