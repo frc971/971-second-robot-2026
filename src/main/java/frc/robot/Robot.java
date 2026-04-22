62b53edf16fe528f48b88dd4f2fe6774d77c6b44
Nikolai Sohmers <nikolai@sohmers.com>
2026-04-21 17:04:54 -0700

    remove coast dt

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,7 +4,6 @@
 
 package frc.robot;
 
-import com.ctre.phoenix6.signals.NeutralModeValue;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj.DataLogManager;
 import edu.wpi.first.wpilibj.DriverStation;
@@ -65,8 +64,6 @@ public class Robot extends LoggedRobot {
     robotContainer = new RobotContainer();
     bos = new BOS(robotContainer.drivetrain);
     autos = new Autos(robotContainer.drivetrain);
-
-    robotContainer.drivetrain.setModuleNeutralMode(NeutralModeValue.Coast);
   }
 
   @Override
