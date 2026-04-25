2c6d493d951b8e28eab6de89445409b2dc78b7b9
Nikolai Sohmers <nikolai@sohmers.com>
2026-04-24 21:57:31 -0700

    Make shooting somewhat work again

--- src/main/java/frc/robot/subsystems/superstructure/Superstructure.java
+++ src/main/java/frc/robot/subsystems/superstructure/Superstructure.java
@@ -173,7 +173,7 @@ public class Superstructure {
         }
       }
 
-      if (!Controllers.INTAKE_PIVOT.toggled()) {
+      if (Controllers.INTAKE_PIVOT.toggled()) {
         setGoal(SetpointGoal.INTAKE_PIVOT);
 
         if (Controllers.JUICE.getAsBoolean()) {
@@ -187,7 +187,7 @@ public class Superstructure {
 
       if (Controllers.INTAKE_ROLLERS.getAsBoolean()) {
         setGoal(SetpointGoal.INTAKE_ROLLERS);
-        groundPivot.setFeedforward(Volts.of(-0.8));
+        groundPivot.setFeedforward(Volts.of(-0.4));
       } else {
         groundPivot.setFeedforward(Volts.of(0.0));
       }
