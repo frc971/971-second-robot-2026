679cd8d3fc98112ca04aa671b7de74c678cd89cb
Nikolai Sohmers <nikolai@sohmers.com>
2026-04-30 08:20:46 -0700

    Current saving changes

--- src/main/java/frc/robot/generated/TunerConstants.java
+++ src/main/java/frc/robot/generated/TunerConstants.java
@@ -66,7 +66,7 @@ public class TunerConstants {
                   .withStatorCurrentLimitEnable(true)
                   .withSupplyCurrentLimitEnable(true)
                   .withStatorCurrentLimit(80)
-                  .withSupplyCurrentLimit(10));
+                  .withSupplyCurrentLimit(20));
   private static final TalonFXConfiguration steerInitialConfigs =
       new TalonFXConfiguration()
           .withCurrentLimits(
