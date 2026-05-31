b0df09098445281fcad18775f87d889833e7f32f
yasen5 <167659798+yasen5@users.noreply.github.com>
2026-05-31 12:44:39 -0700

    Not invert rotation

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -34,23 +34,20 @@ public class RobotContainer {
   private static final double SHUTTLING_ANGULAR_RATE = 0.5 * MAX_ANGULAR_RATE;
 
   /* Setting up bindings for necessary control of the swerve drive platform */
-  private final SwerveRequest.FieldCentric shootingDrive =
-      new SwerveRequest.FieldCentric()
-          .withDeadband(SHOOTING_SPEED * TRANSLATION_DEADBAND)
-          .withRotationalDeadband(SHOOTING_ANGULAR_RATE * ROTATION_DEADBAND)
-          .withDriveRequestType(DriveRequestType.OpenLoopVoltage);
+  private final SwerveRequest.FieldCentric shootingDrive = new SwerveRequest.FieldCentric()
+      .withDeadband(SHOOTING_SPEED * TRANSLATION_DEADBAND)
+      .withRotationalDeadband(SHOOTING_ANGULAR_RATE * ROTATION_DEADBAND)
+      .withDriveRequestType(DriveRequestType.OpenLoopVoltage);
 
-  private final SwerveRequest.FieldCentric shuttlingDrive =
-      new SwerveRequest.FieldCentric()
-          .withDeadband(SHUTTLING_SPEED * TRANSLATION_DEADBAND)
-          .withRotationalDeadband(SHUTTLING_ANGULAR_RATE * ROTATION_DEADBAND)
-          .withDriveRequestType(DriveRequestType.OpenLoopVoltage);
+  private final SwerveRequest.FieldCentric shuttlingDrive = new SwerveRequest.FieldCentric()
+      .withDeadband(SHUTTLING_SPEED * TRANSLATION_DEADBAND)
+      .withRotationalDeadband(SHUTTLING_ANGULAR_RATE * ROTATION_DEADBAND)
+      .withDriveRequestType(DriveRequestType.OpenLoopVoltage);
 
-  private final SwerveRequest.FieldCentric drive =
-      new SwerveRequest.FieldCentric()
-          .withDeadband(MAX_SPEED * TRANSLATION_DEADBAND)
-          .withRotationalDeadband(MAX_ANGULAR_RATE * ROTATION_DEADBAND)
-          .withDriveRequestType(DriveRequestType.OpenLoopVoltage);
+  private final SwerveRequest.FieldCentric drive = new SwerveRequest.FieldCentric()
+      .withDeadband(MAX_SPEED * TRANSLATION_DEADBAND)
+      .withRotationalDeadband(MAX_ANGULAR_RATE * ROTATION_DEADBAND)
+      .withDriveRequestType(DriveRequestType.OpenLoopVoltage);
 
   private final SwerveRequest.SwerveDriveBrake brake = new SwerveRequest.SwerveDriveBrake();
 
@@ -77,18 +74,12 @@ public class RobotContainer {
   private static final SlewRateLimiter Y_LIMITER = new SlewRateLimiter(SLEW_TRANSLATE_LIMIT);
   private static final SlewRateLimiter ROT_LIMITER = new SlewRateLimiter(SLEW_ROTATION_LIMIT);
 
-  private static final SlewRateLimiter SHOOTING_X_LIMITER =
-      new SlewRateLimiter(SHOOTING_SLEW_TRANSLATE_LIMIT);
-  private static final SlewRateLimiter SHOOTING_Y_LIMITER =
-      new SlewRateLimiter(SHOOTING_SLEW_TRANSLATE_LIMIT);
-  private static final SlewRateLimiter SHOOTING_ROT_LIMITER =
-      new SlewRateLimiter(SHOOTING_SLEW_ROTATION_LIMIT);
-  private static final SlewRateLimiter SHUTTLING_X_LIMITER =
-      new SlewRateLimiter(SHUTTLING_SLEW_TRANSLATE_LIMIT);
-  private static final SlewRateLimiter SHUTTLING_Y_LIMITER =
-      new SlewRateLimiter(SHUTTLING_SLEW_TRANSLATE_LIMIT);
-  private static final SlewRateLimiter SHUTTLING_ROT_LIMITER =
-      new SlewRateLimiter(SHUTTLING_SLEW_ROTATION_LIMIT);
+  private static final SlewRateLimiter SHOOTING_X_LIMITER = new SlewRateLimiter(SHOOTING_SLEW_TRANSLATE_LIMIT);
+  private static final SlewRateLimiter SHOOTING_Y_LIMITER = new SlewRateLimiter(SHOOTING_SLEW_TRANSLATE_LIMIT);
+  private static final SlewRateLimiter SHOOTING_ROT_LIMITER = new SlewRateLimiter(SHOOTING_SLEW_ROTATION_LIMIT);
+  private static final SlewRateLimiter SHUTTLING_X_LIMITER = new SlewRateLimiter(SHUTTLING_SLEW_TRANSLATE_LIMIT);
+  private static final SlewRateLimiter SHUTTLING_Y_LIMITER = new SlewRateLimiter(SHUTTLING_SLEW_TRANSLATE_LIMIT);
+  private static final SlewRateLimiter SHUTTLING_ROT_LIMITER = new SlewRateLimiter(SHUTTLING_SLEW_ROTATION_LIMIT);
 
   private static final JoystickValues JOYSTICK_VALUES = new JoystickValues();
 
@@ -104,7 +95,8 @@ public class RobotContainer {
 
     drivetrain.registerTelemetry(logger::telemeterize);
 
-    if (Robot.isSimulation()) drivetrain.resetPose(new Pose2d(3, 3, Rotation2d.kZero));
+    if (Robot.isSimulation())
+      drivetrain.resetPose(new Pose2d(3, 3, Rotation2d.kZero));
 
     FollowPath.registerEventTrigger("shoot", superstructure.shootAuto());
     FollowPath.registerEventTrigger("shootNoJuice", superstructure.shootAutoNoJuice());
@@ -118,10 +110,9 @@ public class RobotContainer {
     drivetrain.setDefaultCommand(
         drivetrain.applyRequest(
             () -> {
-              boolean wantsDrive =
-                  Math.abs(Controllers.TROY.getLeftY()) > TRANSLATION_DEADBAND
-                      || Math.abs(Controllers.TROY.getLeftX()) > TRANSLATION_DEADBAND
-                      || Math.abs(Controllers.TROY.getRightX()) > ROTATION_DEADBAND;
+              boolean wantsDrive = Math.abs(Controllers.TROY.getLeftY()) > TRANSLATION_DEADBAND
+                  || Math.abs(Controllers.TROY.getLeftX()) > TRANSLATION_DEADBAND
+                  || Math.abs(Controllers.TROY.getRightX()) > ROTATION_DEADBAND;
 
               if (Controllers.SHOOTING.getAsBoolean() && !wantsDrive) {
                 return brake;
@@ -156,7 +147,7 @@ public class RobotContainer {
                     .setValues(
                         -Controllers.TROY.getLeftY(),
                         -Controllers.TROY.getLeftX(),
-                        -Controllers.TROY.getRightX())
+                        Controllers.TROY.getRightX())
                     .exponentialCurve(TRANSLATION_EXP_CURVE, ROTATION_EXP_CURVE)
                     .scale(
                         -SHUTTLING_SPEED, // Negative max speed and angular rate since
@@ -174,7 +165,7 @@ public class RobotContainer {
                     .setValues(
                         -Controllers.TROY.getLeftY(),
                         -Controllers.TROY.getLeftX(),
-                        -Controllers.TROY.getRightX())
+                        Controllers.TROY.getRightX())
                     .exponentialCurve(TRANSLATION_EXP_CURVE, ROTATION_EXP_CURVE)
                     .scale(
                         -SHOOTING_SPEED, // Negative max speed and angular rate since
@@ -191,7 +182,7 @@ public class RobotContainer {
                     .setValues(
                         -Controllers.TROY.getLeftY(),
                         -Controllers.TROY.getLeftX(),
-                        -Controllers.TROY.getRightX())
+                        Controllers.TROY.getRightX())
                     .exponentialCurve(TRANSLATION_EXP_CURVE, ROTATION_EXP_CURVE)
                     .scale(
                         -MAX_SPEED, // Negative max speed and angular rate since
