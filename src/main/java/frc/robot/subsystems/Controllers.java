1bb9b387ebdbb675c137aae5e8a57b1c0123c927
Aydin-Firoozshahian <aydin26f@gmail.com>
2026-06-03 11:02:17 -0700

    Rename ANDRE to MANIPULATOR and TROY to DRIVER

--- src/main/java/frc/robot/subsystems/Controllers.java
+++ src/main/java/frc/robot/subsystems/Controllers.java
@@ -6,57 +6,57 @@ import frc.robot.lib.superstructure.Edge;
 import frc.robot.lib.superstructure.Toggle;
 
 public class Controllers {
-  public static final CommandXboxController TROY = new CommandXboxController(0);
-  public static final CommandXboxController ANDRE = new CommandXboxController(1);
+  public static final CommandXboxController DRIVER = new CommandXboxController(0);
+  public static final CommandXboxController MANIPULATOR = new CommandXboxController(1);
 
-  // === Troy's Controls
-  public static final Toggle INTAKE_PIVOT = new Toggle(TROY.leftBumper());
-  public static final Trigger JUICE = ANDRE.a();
-  public static final Edge INTAKE_PIVOT_EDGE = new Edge(TROY.leftBumper());
-  public static final Trigger INTAKE_ROLLERS = TROY.leftTrigger();
-  public static final Trigger UNJAM = ANDRE.leftStick();
+  // === Driver's Controls
+  public static final Toggle INTAKE_PIVOT = new Toggle(DRIVER.leftBumper());
+  public static final Trigger JUICE = MANIPULATOR.a();
+  public static final Edge INTAKE_PIVOT_EDGE = new Edge(DRIVER.leftBumper());
+  public static final Trigger INTAKE_ROLLERS = DRIVER.leftTrigger();
+  public static final Trigger UNJAM = MANIPULATOR.leftStick();
 
-  public static final Trigger SHOOT_REDUNDANCY = TROY.axisGreaterThan(3, 0.9);
-  public static final Trigger INDEX = TROY.rightTrigger();
+  public static final Trigger SHOOT_REDUNDANCY = DRIVER.axisGreaterThan(3, 0.9);
+  public static final Trigger INDEX = DRIVER.rightTrigger();
 
-  public static final Trigger ODOMETRY_RESET = TROY.y();
+  public static final Trigger ODOMETRY_RESET = DRIVER.y();
 
-  // === Andre's Controls
-  public static final Trigger LEFT_SHUTTLE = ANDRE.leftBumper();
-  public static final Trigger RIGHT_SHUTTLE = ANDRE.rightBumper();
-  public static final Trigger SHOOT = ANDRE.axisGreaterThan(3, 0.9);
+  // === Manipulator's Controls
+  public static final Trigger LEFT_SHUTTLE = MANIPULATOR.leftBumper();
+  public static final Trigger RIGHT_SHUTTLE = MANIPULATOR.rightBumper();
+  public static final Trigger SHOOT = MANIPULATOR.axisGreaterThan(3, 0.9);
   public static final Trigger SHOOTING = SHOOT_REDUNDANCY.or(SHOOT);
   public static final Trigger SHUTTLING = LEFT_SHUTTLE.or(RIGHT_SHUTTLE).and(SHOOTING.negate());
 
   public static final Edge SHOOT_EDGE = new Edge(SHOOTING);
   public static final Edge SHUTTLE_EDGE = new Edge(SHUTTLING);
 
-  public static final Trigger OUTTAKE = ANDRE.leftTrigger();
+  public static final Trigger OUTTAKE = MANIPULATOR.leftTrigger();
 
-  public static final Toggle MANUAL = new Toggle(ANDRE.y());
-  public static final Trigger DISABLE_OTF = ANDRE.x();
+  public static final Toggle MANUAL = new Toggle(MANIPULATOR.y());
+  public static final Trigger DISABLE_OTF = MANIPULATOR.x();
 
-  public static final Trigger SHUTTLE_LEFT = ANDRE.leftBumper();
-  public static final Trigger SHUTTLE_RIGHT = ANDRE.rightBumper();
+  public static final Trigger SHUTTLE_LEFT = MANIPULATOR.leftBumper();
+  public static final Trigger SHUTTLE_RIGHT = MANIPULATOR.rightBumper();
 
-  // OTF Tuning (AndreTuner)
-  public static final Trigger TUNE_LEFT = ANDRE.back();
-  public static final Trigger TUNE_RIGHT = ANDRE.start();
+  // OTF Tuning
+  public static final Trigger TUNE_LEFT = MANIPULATOR.back();
+  public static final Trigger TUNE_RIGHT = MANIPULATOR.start();
 
-  public static final Edge FLYWHEEL_UP = new Edge(ANDRE.povUp());
-  public static final Edge FLYWHEEL_DOWN = new Edge(ANDRE.povDown());
-  public static final Edge TURRET_LEFT = new Edge(ANDRE.povLeft());
-  public static final Edge TURRET_RIGHT = new Edge(ANDRE.povRight());
+  public static final Edge FLYWHEEL_UP = new Edge(MANIPULATOR.povUp());
+  public static final Edge FLYWHEEL_DOWN = new Edge(MANIPULATOR.povDown());
+  public static final Edge TURRET_LEFT = new Edge(MANIPULATOR.povLeft());
+  public static final Edge TURRET_RIGHT = new Edge(MANIPULATOR.povRight());
 
   // MANUAL Shooter Setpoints
-  public static final Trigger MANUAL_SHOOT_UP = ANDRE.axisLessThan(1, -0.85);
-  public static final Trigger MANUAL_SHOOT_DOWN = ANDRE.axisGreaterThan(1, 0.85);
-  public static final Trigger MANUAL_SHOOT_LEFT = ANDRE.axisLessThan(0, -0.85);
-  public static final Trigger MANUAL_SHOOT_RIGHT = ANDRE.axisGreaterThan(0, 0.85);
+  public static final Trigger MANUAL_SHOOT_UP = MANIPULATOR.axisLessThan(1, -0.85);
+  public static final Trigger MANUAL_SHOOT_DOWN = MANIPULATOR.axisGreaterThan(1, 0.85);
+  public static final Trigger MANUAL_SHOOT_LEFT = MANIPULATOR.axisLessThan(0, -0.85);
+  public static final Trigger MANUAL_SHOOT_RIGHT = MANIPULATOR.axisGreaterThan(0, 0.85);
 
   // MANUAL Shuttle Setpoints
-  public static final Trigger MANUAL_SHUTTLE_UP = ANDRE.axisLessThan(5, -0.85);
-  public static final Trigger MANUAL_SHUTTLE_DOWN = ANDRE.axisGreaterThan(5, 0.85);
-  public static final Trigger MANUAL_SHUTTLE_LEFT = ANDRE.axisLessThan(4, -0.85);
-  public static final Trigger MANUAL_SHUTTLE_RIGHT = ANDRE.axisGreaterThan(4, 0.85);
+  public static final Trigger MANUAL_SHUTTLE_UP = MANIPULATOR.axisLessThan(5, -0.85);
+  public static final Trigger MANUAL_SHUTTLE_DOWN = MANIPULATOR.axisGreaterThan(5, 0.85);
+  public static final Trigger MANUAL_SHUTTLE_LEFT = MANIPULATOR.axisLessThan(4, -0.85);
+  public static final Trigger MANUAL_SHUTTLE_RIGHT = MANIPULATOR.axisGreaterThan(4, 0.85);
 }
